cf3437a41cb00b1b0ddc659a1e4dfc397f6f2e62
Eduardo Escaleira <duescaleira@gmail.com>
2023-03-16 13:19:09 -0300

    xcode configurado para viewCode

---
+++ PrimeiroAppMVVM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PrimeiroAppMVVM
+//
+//  Created by Eduardo Escaleira on 16/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
